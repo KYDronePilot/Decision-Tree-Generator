66cbb372108197af7ba3191f01a8655a4ac49428
KYDronePilot <kydronepilot2016@gmail.com>
2018-12-28 19:34:31 -0500

    Code is now functional, with some examples of algorithms which it accurately prints the decisions trees for.
    More work will need to be done to clean up the Latex code that is
    printed out, as well as clarify/clean all of the Python code.

--- tree_gen.py
+++ tree_gen.py
@@ -1,9 +1,9 @@
 # Tree nodes that will print out the LaTeX code.
 class LatexNode:
     # Template for node text.
-    NODE_TEMPLATE = '{node}, edge label={{node[midway,fill=white,font=\\tiny] {{{edge}}}}}'
+    NODE_TEMPLATE = '${node}$, edge label={{node[midway,fill=white,font=\\tiny] {{{edge}}}}}'
 
-    def __init__(self, val='', right=None, left=None, is_leaf=False, edge_label='', left_side=True):
+    def __init__(self, val='', right=None, left=None, is_leaf=False, left_side=True, root=False):
         """
 
         :type right: LatexNode
@@ -14,8 +14,12 @@ class LatexNode:
         self.right = right
         self.left = left
         self.is_leaf = is_leaf
-        self.edge_label = edge_label
         self.left_side = left_side
+        # Determine edge label.
+        if root:
+            self.edge_label = ''
+        else:
+            self.edge_label = 'Yes' if left_side else 'No'
 
     # def render(self):
     #     # If node is leaf, prepare leaf text accordingly.
@@ -36,15 +40,15 @@ class LatexNode:
 
     def render_forest(self):
         #print('[ ', end='')
-        global text
-        text += '[ '
+        global latex_code
+        latex_code += '[ '
         # If this is a pruned node, do not put in any contents.
         if not 'null' == self.val:
             #print(LatexNode.NODE_TEMPLATE.format(node=self.val, edge=self.edge_label), end=' ')
-            text += LatexNode.NODE_TEMPLATE.format(node=self.val, edge=self.edge_label)
+            latex_code += LatexNode.NODE_TEMPLATE.format(node=self.val, edge=self.edge_label)
         else:
             #print(', phantom', end=' ')
-            text += ', phantom'
+            latex_code += ', phantom'
         # Recursively render children if they exist.
         if self.left is not None:
             self.left.render_forest()
@@ -52,7 +56,7 @@ class LatexNode:
             self.right.render_forest()
         # Add closing bracket.
         #print(' ]', end='')
-        text += ' ]'
+        latex_code += ' ]'
         pass
 
 
@@ -67,6 +71,14 @@ class Stack:
     def __len__(self):
         return self._n
 
+    def is_empty(self):
+        """
+
+        Returns: Whether or not stack is empty.
+
+        """
+        return self._n == 0
+
     # Push element to the stack.
     def push(self, elem):
         self._data.append(elem)
@@ -80,6 +92,50 @@ class Stack:
     # Get element on top.
     def top(self):
         return self._data[-1]
+
+    def copy(self):
+        """
+        Deep copy the Stack.
+
+        Returns: New Stack instance.
+
+        """
+        new_stack = Stack()
+        new_stack._data = self._data.copy()
+        new_stack._n = self._n
+        return new_stack
+
+    # Iterator from top to bottom.
+    def __iter__(self):
+        for i in range(len(self._data) - 1, -1, -1):
+            yield self._data[i]
+
+    # def __getitem__(self, item):
+    #     """
+    #     Reverse slicer for stack's data object.
+    #
+    #     Args:
+    #         item (slice): The slice.
+    #
+    #     Returns: Reverse sliced list.
+    #
+    #     """
+    #     new_start = item.stop - 1 if item.stop is not None else None
+    #     new_end = item.start - 1 if item.start is not None else None
+    #     new_slice = slice(new_start, new_end, -1)
+    #     return self._data[new_slice]
+
+    def __getitem__(self, item):
+        """
+        Slicer for the main data object.
+
+        Args:
+            item (slice): The slice.
+
+        Returns: Sliced data list.
+
+        """
+        return self._data[item]
 
 
 # Lightweight queue ADT.
@@ -122,7 +178,7 @@ class Queue:
         if self.is_empty():
             self._head = new
         else:
-            self._tail._next = new
+            self._tail.next_elem = new
         self._tail = new
         self._size += 1
 
@@ -140,13 +196,21 @@ class Comparison:
     def __init__(self, e1, op, e2):
         """
 
-        :param e1: The first element
-        :param op: The operator
-        :param e2: The second element
+        Args:
+            e1: The first element.
+            op: The operator.
+            e2: The second element.
         """
         self.e1 = e1
         self.op = op
         self.e2 = e2
+
+    def __str__(self):
+        return '{0} {1} {2}'.format(
+            self.e1,
+            self.op,
+            self.e2
+        )
 
     def __eq__(self, other):
         """
@@ -159,16 +223,16 @@ class Comparison:
             True if equivalent, False if not.
 
         """
-        # Other operator.
-        other_op = other.op
         # If operators not equal, get the negation of one and check again.
-        if self.op != other_op:
-            other_op = Comparison.NEGATIONS[other_op]
+        if self.op != other.op:
+            other_op = Comparison.NEGATIONS[other.op]
             # If still not equal, comparisons cannot be equivalent.
             if self.op != other_op:
                 return False
-        # TODO: finish here.
-
+            # If comparison equal to negation, check cross elements.
+            return self.e1 == other.e2 and self.e2 == other.e1
+        # If operators are equal, then equivalence is based on elements.
+        return self.e1 == other.e1 and self.e2 == other.e2
 
     def negate(self):
         """
@@ -184,70 +248,264 @@ class Comparison:
 
 # Information about the state of execution at a branch.
 class State:
-    def __init__(self, previous, decision, list_state, node):
+    def __init__(self, node, comp):
         # The previous state.
-        self.previous = previous
-        # Decision made to get here from last state.
-        self.decision = decision
-        # State of list before making decision at this branch.
-        self.list_state = list_state
+        #self.previous = previous
+        # Last decision made at this state.
+        self.last_decision = True
         # LatexNode at this branch.
         self.node = node
+        # Comparison made at this node.
+        self.comp = comp
 
 
 # Main tree generation.
 class TreeGenerator:
-    def __init__(self):
+    def __init__(self, base_data):
         # The last state of this program.
         self.last_state = None
         # Whether the algorithm is in the process of restoring a state or not.
         self.restoring = False
+        # Whether the algorithm is in the process of running out its course.
+        self.lame_duck = False
+        # Decisions to get back to a state.
+        self.restoring_decisions = Queue()
+        # First decision to make after a restoration.
+        self.first_decision = True
+        # States to get to the current execution point.
+        self.states = Stack()
+        # The list being manipulated.
+        self.data = []
+        # The root LatexNode.
+        self.root_latex = None
+        # Base data set being worked with.
+        self.base_data = base_data
 
-    # Get a stack of decisions to get to a state.
-    def get_decisions(self, state, decisions):
+    def get_restoring_decisions(self):
         """
+        Get decisions to restore and start down left branch of state in question.
 
-        :type state: State
-        :type decisions: Stack
+        Returns: TODO: Nothing?
+
         """
-        # Stop when root state is reached.
-        if state.decision is None:
-            return decisions
-        # Add state's decision to the stack and recur.
-        decisions.push(state.decision)
-        return self.get_decisions(state.previous, decisions)
+        # Enqueue each decision into restoring decisions.
+        for state in self.states[:-1]:
+            self.restoring_decisions.enqueue(state.last_decision)
+        # Start exploring right branch of state, as last of restoration decisions.
+        self.restoring_decisions.enqueue(False)
 
     # Algorithm execution manager.
     def execution_manager(self):
-        # Run the algorithm till it ends its execution.
-        self.algorithm()
-        # Get the last pushed state and get back to that branch point.
-        last_state = self.decisions.pop()
+        # Initialize data list.
+        self.data = self.base_data.copy()
+        # Execute algorithm until every possible path has been taken.
+        while True:
+            # Run algorithm.
+            self.algorithm()
+            # Terminate with blank node if last comparison is invalid.
+            self.check_valid()
+            # If algorithm was not already terminated, add leaf.
+            if not self.lame_duck:
+                self.terminate_path(is_blank=False)
+            # Find the next branch to analyze.
+            self.find_branch()
+            # Exit once all paths taken.
+            if self.states.is_empty():
+                break
+            # Get decisions to be made to get to that branch.
+            self.get_restoring_decisions()
+            pass
 
-    # Handle a comparison (branch).
+    def find_branch(self):
+        """
+        Pop states until a state who's right path has not been explored is found.
+
+        Returns:
+
+        """
+        while not self.states.is_empty() and not self.states.top().last_decision:
+            self.states.pop()
+        # If all states have been popped, exit.
+        if self.states.is_empty():
+            return
+        # Put in restoring mode and out of lame duck mode.
+        self.restoring = True
+        self.lame_duck = False
+        # Get the state in question.
+        state = self.states.top()
+        # Update the last decision made and the comparison outcome at this state.
+        new_comp = state.comp.negate()
+        state.last_decision = False
+        state.comp = new_comp
+        # Change data back to the initial base data.
+        self.data = self.base_data.copy()
+        # TODO: change 'comp' to something like 'truth'.
+
+    def add_node(self, new_comp):
+        """
+        Add a node to the execution path.
+
+        Args:
+            new_comp (Comparison): Comparison made at this node.
+
+        Returns:
+
+        """
+        # Get latest state.
+        latest_state = self.states.top()
+        # Create new LatexNode.
+        text = str(new_comp)
+        is_left = latest_state.last_decision
+        new_node = LatexNode(
+            val=text,
+            is_leaf=False,
+            left_side=is_left
+        )
+        # Link node at latest state to this new node.
+        latest_node = latest_state.node
+        if is_left:
+            latest_node.left = new_node
+        else:
+            latest_node.right = new_node
+        # Get a new state and push to states.
+        new_state = State(new_node, new_comp)
+        self.states.push(new_state)
+
+    def terminate_path(self, is_blank):
+        """
+        Terminate an execution path.
+
+        Args:
+            is_blank (bool): Whether or not the node is blank.
+
+        Returns:
+
+        """
+        # Get latest state.
+        latest_state = self.states.top()
+        # Get a leaf LatexNode.
+        is_left = latest_state.last_decision
+        # TODO: change formatting.
+        text = 'null' if is_blank else ' '.join(self.data)
+        new_node = LatexNode(
+            val=text,
+            is_leaf=True,
+            left_side=is_left
+        )
+        # Link node at latest state to this new node.
+        latest_node = latest_state.node
+        if is_left:
+            latest_node.left = new_node
+        else:
+            latest_node.right = new_node
+        # Let algorithm run its course.
+        self.lame_duck = True
+
+    def first_comp(self, new_comp):
+        """
+        Handle the first comparison (special case).
+
+        Args:
+            new_comp (Comparison): The comparison made at root.
+
+        Returns:
+
+        """
+        # Create root LatexNode.
+        text = str(new_comp)
+        new_node = LatexNode(
+            val=text,
+            root=True
+        )
+        # Get a new state and push to states.
+        new_state = State(new_node, new_comp)
+        self.states.push(new_state)
+        # Set the root LatexNode.
+        self.root_latex = new_node
+
+    def check_valid(self):
+        """
+        Check if current point is valid.
+
+        Returns: True if valid, False if not.
+
+        """
+        # Get last comparison.
+        last_comp = self.states.top().comp
+        # Check if negation was a previous comparison and terminate if so.
+        if last_comp.negate() in (x.comp for x in self.states[:-1]):
+            self.terminate_path(is_blank=True)
+            return False
+        return True
+
     def comp(self, e1, op, e2):
-        # State at
-        pass
+        """
+        Handle a comparison (branch).
+
+        Args:
+            e1: First element.
+            op: Operator.
+            e2: Second element.
+
+        Returns: TODO: Nothing?
+
+        """
+        # If algorithm is running its course, always return True.
+        if self.lame_duck:
+            return True
+        # If restoring to execution state, make next required decision.
+        if self.restoring:
+            decision = self.restoring_decisions.dequeue()
+            # If no more decisions left, end restoring mode.
+            if self.restoring_decisions.is_empty():
+                self.restoring = False
+            return decision
+        # Comparison being made.
+        comp = Comparison(e1, op, e2)
+        # Handle first comparison special case and branch left.
+        if self.states.is_empty():
+            self.first_comp(comp)
+            return True
+        # Get latest state.
+        latest_state = self.states.top()
+        # Terminate path if last comparison was invalid.
+        if not self.check_valid():
+            return True
+        # Add new node to the execution path.
+        self.add_node(comp)
+        # Continue down left branch.
+        return True
 
     # Algorithm method, overridden in inheritor.
     def algorithm(self):
         raise NotImplementedError('This method should be overridden with an algorithm implementation.')
 
 
+# Example Bubble Sort algorithm.
+class BubbleSort(TreeGenerator):
+    def algorithm(self):
+        for i in range(len(self.data) - 2, -1, -1):
+            for j in range(i + 1):
+                if self.comp(self.data[j], '>', self.data[j + 1]):
+                    tmp = self.data[j]
+                    self.data[j] = self.data[j + 1]
+                    self.data[j + 1] = tmp
 
 
+class InsertionSort(TreeGenerator):
+    def algorithm(self):
+        for i in range(1, len(self.data)):
+            elem = self.data[i]
+            j = i - 1
+            while j >= 0 and self.comp(self.data[j], '>', elem):
+                self.data[j + 1] = self.data[j]
+                j -= 1
+            self.data[j + 1] = elem
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    tree_gen = InsertionSort(['a', 'b', 'c'])
+    latex_code = ''
+    tree_gen.execution_manager()
+    tree_gen.root_latex.render_forest()
+    print(latex_code)
